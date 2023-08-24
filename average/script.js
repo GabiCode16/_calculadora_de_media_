fdf0afb5767423648c2b75169cc42a2c8e0da0e4
Gabriel Frizzo Ciervo <91836087+GabiCode16@users.noreply.github.com>
2023-08-24 15:07:20 -0300

    Update

--- average/script.js
+++ average/script.js
@@ -1,14 +1,27 @@
-function start() {
-    var divText = document.getElementById("container");
+var divText = document.getElementById("container");
 
-var page2 = "<h1>Calculadora de Média</h1>\n" +
+function start() { 
+    var page2 = "<h1>Calculadora de Média</h1>\n" +
     "<select id='media'>\n" +
-        "<option value='' selected>Selecione a Média\n" +
+        "<option value='' selected disabled>Selecione a Média\n" +
         "<option value='01'>Média Aritmética\n" +
         "<option value='02'>Média Ponderada\n" +
         "<option value='03'>Média Harmônica\n" +
     "</select><br><br><br>\n" +
-    "<button onclick='start()'>Começar</button>";
+    "<button onclick='select()'>Começar</button>";
 
     divText.innerHTML = page2;
+}
+
+function select() {
+    var page3 = "<h1>Calculadora de Média</h1>\n\
+    <select id='media'>\n\
+    <option value='' selected disabled>Selecione o modo de avaliação\n\
+    <option value='b'>Bimestre\n\
+    <option value='t'>Trimestre\n\
+    <option value='s'>Semestre\n\
+    </select><br><br><br>\n\
+    <button onclick='select2()'>Começar</button>";
+
+    divText.innerHTML = page3;
 }
